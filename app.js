ce76fcb980b0e6d6b9df9bed9423bb69d2c5326b
Leo <leozinho17silvacosta@gmail.com>
2025-11-24 20:50:45 -0800

    Complete JQuery Exercise - WEB 2

--- app.js
+++ app.js
@@ -1,3 +1,58 @@
+
+// EXERCISE 1:
+
 $(function() {
-  // YOUR CODE HERE
+   $('#toggleButton').on('click', function() {
+    $('#myParagraph').toggle();
+  })
+})
+
+// EXERCISE 2:
+
+$(function () {
+  $('#colorButton').on('click', function() {
+    $('#colorDiv').css('background-color', 'yellow');
+  })
+})
+
+// EXERCISE 3:
+
+$(function () {
+  $('#addClassButton').on('click', function() {
+    $('#classDiv').addClass('newClass');
+  })
+})
+
+$(function () {
+  $('#removeClassButton').on('click', function() {
+    $('#classDiv').removeClass('newClass');
+  })
+})
+
+// EXERCISE 4:
+
+$(function () {
+  $('#fadeInButton').on('click', function() {
+    $('#fadeDiv').fadeIn(500);
+  })
+})
+
+$(function () {
+  $('#fadeOutButton').on('click', function() {
+    $('#fadeDiv').fadeOut(500);
+  })
+})
+
+// EXERCISE 5:
+
+$(function () {
+  $('#slideUpButton').on('click', function() {
+    $('#slideDiv').slideUp(500);
+  })
+})
+
+$(function () {
+  $('#slideDownButton').on('click', function() {
+    $('#slideDiv').slideDown(500);
+  })
 })
